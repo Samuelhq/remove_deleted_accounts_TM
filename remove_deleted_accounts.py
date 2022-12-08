efea668ee013ef4c2f701973ca238ad6b4382cc7
Samuelhq <105362552+Samuelhq@users.noreply.github.com>
2022-12-08 14:19:45 +0100

    Update remove_deleted_accounts.py

--- remove_deleted_accounts.py
+++ remove_deleted_accounts.py
@@ -21,22 +21,40 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I am RDA_bot, a bot that removes deleted accounts from a Telegram group.")
 
-# Define a command handler for the /remove command, which removes deleted accounts from the group
-def remove(update, context):
-    # Get a list of all members of the group
-    members = context.bot.get_chat_members_count(chat_id=GROUP_ID)
-
-    # Loop through each member of the group
-    for member in members:
-        # If the member's account has been deleted, kick them from the group
-        if member.user.is_deleted:
-            context.bot.kick_chat_member(chat_id=GROUP_ID, user_id=member.user.id, until_date=0)
-
 # Add the command handlers to the dispatcher
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
 remove_handler = CommandHandler("remove", remove)
 dispatcher.add_handler(remove_handler)
+    
+    
+    # Define a command handler for the /remove command, which removes deleted accounts from the group
+def remove(update, context):
+    # Get a list of all members of the group
+    members = context.bot.get_chat_members_count(chat_id=GROUP_ID)
+
+  # Count how many members have deleted their account
+  deleted_accounts = 0
+  for member in members:
+    if member.user.is_deleted:
+      deleted_accounts += 1
+
+  # Send a message to the user with the number of deleted accounts
+  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Removed {deleted_accounts} deleted accounts from the group.")
+
+# Define a command handler for the /remove command, which removes deleted accounts from the group
+def remove(update, context):
+  # Get a list of all members of the group
+  members = context.bot.get_chat_members_count(chat_id=GROUP_ID)
+
+  # Loop through each member of the group
+  for member in members:
+    # If the member's account has been deleted, kick them from the group
+    if member.user.is_deleted:
+      context.bot.kick_chat_member(chat_id=GROUP_ID, user_id=member.user.id, until_date=0)
+
+# Report how many deleted accounts were removed
+report(update, context)
 
 # Create a job that runs every 24 hours to remove deleted accounts from the group
 job_queue = updater.job_queue
